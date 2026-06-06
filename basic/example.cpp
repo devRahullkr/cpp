48ec9e95e702b5852c44c9b8cbb0534007dca3e0
Rahulllkr <rahulkumar8604745@gmail.com>
2026-06-06 06:42:04 +0530

    boiler plate

--- basic/example.cpp
+++ basic/example.cpp
@@ -1,7 +1,15 @@
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     cout << "Boiler Plate" << endl;
+//     return 0;
+// }
+
 #include <iostream>
 using namespace std;
 
 int main(){
-    cout << "Rahul Kumar" << '\n';
+    cout << "Rahul Kumar " << "From ayodhyadham" << endl;
     return 0;
 }
