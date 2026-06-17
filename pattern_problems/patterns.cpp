5bc3d37cb96a164e9863c389c194f25c7b3bf6c0
Rahulllkr <rahulkumar8604745@gmail.com>
2026-06-17 23:42:02 +0530

    floyd's triangle pattern with numbers and characters

--- pattern_problems/patterns.cpp
+++ pattern_problems/patterns.cpp
@@ -140,16 +140,49 @@ using namespace std;
 
 // reverse numeric triangle
 
-int main(){
-    int n = 5;
-    for(int i = 0 ; i < n ; i++){
-        for(int j = i+1 ; j >= 1 ; j--){
-            cout << j << " ";
-        }
-        cout << endl;
-    }
-    return 0;
-}
+// int main(){
+//     int n = 5;
+//     for(int i = 0 ; i < n ; i++){
+//         for(int j = i+1 ; j >= 1 ; j--){
+//             cout << j << " ";
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
+
+
+// floyd's triangle pattern with numbers
+
+// int main(){
+//     int n = 4;
+//     int num = 1;
+//     for(int i = 0 ; i < n ; i++){
+//         for(int j = 0 ; j <= i ; j++){
+//             cout << num << " ";
+//             num += 1;
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
+
+// floyd's triangle pattern with character
+
+// int main(){
+//     int n = 4;
+//     char ch = 'A';
+//     for(int i = 0 ; i < n ; i++){
+//         for(int j = 0 ; j <= i ; j++){
+//             cout << ch << " ";
+//             ch += 1;
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
+
+
 
 
 
