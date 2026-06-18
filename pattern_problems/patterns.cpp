950923b03af62fee0befa63f681fb4cea0b11dd5
Rahulllkr <rahulkumar8604745@gmail.com>
2026-06-18 23:37:31 +0530

    pyramid pattern with numbers

--- pattern_problems/patterns.cpp
+++ pattern_problems/patterns.cpp
@@ -239,6 +239,29 @@ using namespace std;
 // }
 
 
+// Pyramid pattern with numbers
+
+int main(){
+    int n = 4;
+
+    for(int i = 0 ; i < n ; i++){
+
+        for(int j = 0 ; j < n-1-i ; j++){
+            cout << " ";
+        }
+        for(int j = 1 ; j <= i+1 ; j++){
+            cout << j ;
+        }
+        for(int j = i ; j >= 1 ; j--){
+            cout << j ;
+        }
+        cout << endl;
+
+    }
+    return 0;
+}
+
+
 
 
 
